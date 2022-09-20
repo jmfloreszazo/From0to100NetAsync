09958fd1845abdb0dc48fe0dada723f3b5868b37
Jose Maria Flores Zazo <jmfloreszazo@gmail.com>
2022-09-20 14:36:46 +0200

    Add Thread & ThreadPool diff example
    Add Thread & ThreadPool diff example

---
+++ src/ThreadPoolWithBenchmarks/Program.cs
@@ -0,0 +1,42 @@
+﻿using System.Diagnostics;
+
+for (var i = 0; i < 10; i++)
+{
+    MethodWithThread();
+    MethodWithThreadPool();
+}
+
+var stopwatch = new Stopwatch();
+Console.WriteLine("Execution using Thread");
+stopwatch.Start();
+MethodWithThread();
+stopwatch.Stop();
+Console.WriteLine("Time consumed by MethodWithThread is : " +
+                  stopwatch.ElapsedTicks);
+
+stopwatch.Reset();
+Console.WriteLine("Execution using Thread Pool");
+stopwatch.Start();
+MethodWithThreadPool();
+stopwatch.Stop();
+Console.WriteLine("Time consumed by MethodWithThreadPool is : " +
+                  stopwatch.ElapsedTicks);
+
+Console.Read();
+
+void MethodWithThread()
+{
+    for (var i = 0; i < 10; i++)
+    {
+        var thread = new Thread(TestObject);
+    }
+}
+
+void MethodWithThreadPool()
+{
+    for (var i = 0; i < 10; i++) ThreadPool.QueueUserWorkItem(TestObject);
+}
+
+void TestObject(object obj)
+{
+}
