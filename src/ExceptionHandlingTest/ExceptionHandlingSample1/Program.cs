e00914f8fa9a21289b31474f00a30907f43db6b9
Jose Maria Flores Zazo <jmfloreszazo@gmail.com>
2022-09-21 19:48:37 +0200

    ExceptionHandling
    ExceptionHandling

---
+++ src/ExceptionHandlingTest/ExceptionHandlingSample1/Program.cs
@@ -0,0 +1,19 @@
+﻿//SleepAsyncOne(); //Case 1.A
+//_ = SleepAsyncOne(); //Case 2.B
+//await SleepAsyncOne(); //Case 2.C
+
+//SleepAsynTwo(); //Case 2.A
+//_ = SleepAsynTwo(); //Case 2.B
+await SleepAsynTwo(); //Case 2.C
+
+static async Task SleepAsyncOne()
+{
+    await Task.Delay(100);
+    throw new NullReferenceException();
+}
+
+static async Task<Task> SleepAsynTwo()
+{
+    await Task.Delay(100);
+    return Task.FromException(new NullReferenceException());
+}
