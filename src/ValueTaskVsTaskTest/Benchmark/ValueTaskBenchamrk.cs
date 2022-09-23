220b446143367be3f5a0603f68c85e0d5fbd3026
Jose Maria Flores Zazo <jmfloreszazo@gmail.com>
2022-09-23 08:43:33 +0200

    ValueTaskVsTask
    ValueTaskVsTask

---
+++ src/ValueTaskVsTaskTest/Benchmark/ValueTaskBenchamrk.cs
@@ -0,0 +1,15 @@
+﻿using BenchmarkDotNet.Attributes;
+
+namespace Benchmark;
+
+public class ValueTaskBenchamrk
+{
+    readonly Repository _repo = new Repository();
+
+    [Benchmark]
+    public async Task RunTaskAsync() => await _repo.GetDataWithTaskAsync("test");
+
+
+    [Benchmark]
+    public async Task RunValueTaskAsync() => await _repo.GetDataWithValueTaskAsync("test");
+}
---
+++ src/ValueTaskVsTaskTest/ValueTaskVsTaskTest/Repository.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.Extensions.Caching.Memory;
+
+public class Repository
+{
+    private readonly IMemoryCache _memoryCache = new MemoryCache(new MemoryCacheOptions());
+
+    public async Task<string> GetDataWithTaskAsync(string cacheKey)
+    {
+        var value = _memoryCache.Get<string>(cacheKey);
+        if (value is null)
+        {
+            value = await GetData();
+            _memoryCache.Set(cacheKey, value, TimeSpan.FromHours(1));
+        }
+
+        return value;
+    }
+
+    public async ValueTask<string> GetDataWithValueTaskAsync(string cacheKey)
+    {
+        var value = _memoryCache.Get<string>(cacheKey);
+        if (value is null)
+        {
+            value = await GetData();
+            _memoryCache.Set(cacheKey, value, TimeSpan.FromHours(1));
+        }
+
+        return value;
+    }
+
+    private async Task<string> GetData()
+    {
+        await Task.Delay(500);
+        return "Data";
+    }
+}
